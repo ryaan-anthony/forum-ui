903c2459326b0757ae5d6e11dfd78e2eb50bcbc5
Tulino, Ryan <rtulino@ebay.com>
2017-01-22 17:31:22 -0500

    clean up css/js

--- app/assets/javascripts/templates/content.js
+++ app/assets/javascripts/templates/content.js
@@ -3,12 +3,17 @@
   Template.content = function(url, title, description) {
     var description_container = document.createElement('div');
     $(description_container).addClass('description-container').text(description);
-    var title_link_tag = document.createElement('a');
     var title_container = document.createElement('div');
-    $(title_link_tag).attr('href', url).attr('target', '_blank').text(title);
     return $(title_container)
       .addClass('col col-xs-10 title-container')
-      .append($(title_link_tag))
+      .append(Template.title(title))
+      .append(Template.url(url))
       .append($(description_container));
+  };
+  Template.url = function(url) {
+    return $(document.createElement('a')).attr('href', url).attr('target', '_blank').text(url);
+  };
+  Template.title = function(title) {
+    return $(document.createElement('h5')).text(title);
   };
 }).call(this);
