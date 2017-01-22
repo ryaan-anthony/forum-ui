4ba13636763cc9efa39297cc3c57ffa6847e21af
Tulino, Ryan <rtulino@ebay.com>
2017-01-21 23:40:49 -0500

    resolves #3 quick links

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,8 @@ class Group < ApplicationRecord
   include RandomCode
   belongs_to :author, optional: true
 
+  def can_submit
+    true
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :authors
-  get '/(.:format)', to: 'page#index', as: 'frontpage'
+  get '/(.:format)', to: 'page#index', as: 'homepage'
   authenticate :author do
     resources :groups, only: [:new, :create, :edit, :update, :destroy]
   end
